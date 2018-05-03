9f7b7f2e231fcf587fd9e90165c209ecbff74bb1
Thomas Haller <thaller@redhat.com>
2018-05-03 10:33:10 +0200

    cli: allow marking NMMetaAbstractInfo to be excluded from get-all-fields
    nmcli allows to select output fields with the --fields option.
    Thereby, it recognizes the special names "all" and "common".
    
    "all" is clear, however, for "common" we currently implement by letting
    the caller replace the "common" string with a preselected list of fields,
    for example
    
    »···if (!nmc->required_fields || strcasecmp (nmc->required_fields, "common") == 0)
    »···»···fields_str = NMC_FIELDS_DEV_SHOW_SECTIONS_COMMON;
    
    Note how the list of fields is parsed by parse_output_fields() and
    indirectly nm_meta_selection_create_parse_list(). However, currently
    "common" is treated special, and pre-parsed outside of
    create-parse-list(). That means, we cannot do something like
    
      nmcli -f GENERAL,WIFI-PROPERTIES.COMMON device show wlan0
    
    Allow the field descriptions to indicate themself whether they
    should be included in "common" or not.
    
    This will allow nm_meta_selection_create_parse_list() to do the parsing.

--- clients/cli/utils.c
+++ clients/cli/utils.c
@@ -38,6 +38,24 @@ _meta_type_nmc_generic_info_get_name (const NMMetaAbstractInfo *abstract_info, g
 	return info->name;
 }
 
+static gboolean
+_meta_type_nmc_generic_info_included_in_common (const NMMetaAbstractInfo *abstract_info,
+                                                int *out_order,
+                                                gboolean *out_is_common_parent)
+{
+	const NmcMetaGenericInfo *info = (const NmcMetaGenericInfo *) abstract_info;
+
+	NM_SET_OUT (out_is_common_parent, info->is_common_parent);
+
+	if (info->common_priority < 0) {
+		nm_assert (info->common_priority == -1);
+		NM_SET_OUT (out_order, 0);
+		return FALSE;
+	}
+	NM_SET_OUT (out_order, info->common_priority);
+	return TRUE;
+}
+
 static const NMMetaAbstractInfo *const*
 _meta_type_nmc_generic_info_get_nested (const NMMetaAbstractInfo *abstract_info,
                                         guint *out_len,
@@ -99,10 +117,11 @@ _meta_type_nmc_generic_info_get_fcn (const NMMetaAbstractInfo *abstract_info,
 }
 
 const NMMetaType nmc_meta_type_generic_info = {
-	.type_name =         "nmc-generic-info",
-	.get_name =          _meta_type_nmc_generic_info_get_name,
-	.get_nested =        _meta_type_nmc_generic_info_get_nested,
-	.get_fcn =           _meta_type_nmc_generic_info_get_fcn,
+	.type_name =          "nmc-generic-info",
+	.get_name =           _meta_type_nmc_generic_info_get_name,
+	.included_in_common = _meta_type_nmc_generic_info_included_in_common,
+	.get_nested =         _meta_type_nmc_generic_info_get_nested,
+	.get_fcn =            _meta_type_nmc_generic_info_get_fcn,
 };
 
 /*****************************************************************************/
@@ -673,18 +692,19 @@ _output_selection_append (GArray *cols,
 			.self_idx = col_idx,
 			.is_leaf = TRUE,
 		};
+
 		g_array_append_val (cols, col);
 	}
 
-	nested = nm_meta_abstract_info_get_nested (selection_item->info, NULL, &nested_to_free);
-
 	if (selection_item->sub_selection) {
+		nested = nm_meta_abstract_info_get_nested (selection_item->info, TRUE, NULL, &nested_to_free);
+
 		if (!nested) {
 			gs_free char *allowed_fields = NULL;
 
 			if (parent_idx != PRINT_DATA_COL_PARENT_NIL) {
 				si = g_array_index (cols, PrintDataCol, parent_idx).selection_item;
-				allowed_fields = nm_meta_abstract_info_get_nested_names_str (si->info, si->self_selection);
+				allowed_fields = nm_meta_abstract_info_get_nested_names_str (si->info, TRUE, si->self_selection);
 			}
 			if (!allowed_fields) {
 				g_set_error (error, NMCLI_ERROR, 1, _("invalid field '%s%s%s'; no such field"),
@@ -704,11 +724,17 @@ _output_selection_append (GArray *cols,
 		if (!selection)
 			return FALSE;
 		nm_assert (selection->num == 1);
-	} else if (nested) {
-		selection = nm_meta_selection_create_all (nested);
-		nm_assert (selection && selection->num > 0);
-	} else
-		selection = NULL;
+	} else {
+		nested = nm_meta_abstract_info_get_nested (selection_item->info,
+		                                           FALSE,
+		                                           NULL,
+		                                           &nested_to_free);
+		if (nested) {
+			selection = nm_meta_selection_create_all (nested);
+			nm_assert (selection && selection->num > 0);
+		} else
+			selection = NULL;
+	}
 
 	if (selection) {
 		g_ptr_array_add (gfree_keeper, selection);
@@ -799,10 +825,11 @@ _output_selection_parse (const NMMetaAbstractInfo *const*fields,
 	cols = g_array_new (FALSE, TRUE, sizeof (PrintDataCol));
 
 	for (i = 0; i < selection->num; i++) {
-		const NMMetaSelectionItem *si = &selection->items[i];
-
-		if (!_output_selection_append (cols, PRINT_DATA_COL_PARENT_NIL,
-		                               si, gfree_keeper, error))
+		if (!_output_selection_append (cols,
+		                               PRINT_DATA_COL_PARENT_NIL,
+		                               &selection->items[i],
+		                               gfree_keeper,
+		                               error))
 			return FALSE;
 	}
 
